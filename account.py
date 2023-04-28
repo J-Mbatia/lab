64869cf298b372ce90b36d86901de1b066d13d8d
Jayden <jayden.mbatia@gmail.com>
2023-04-28 16:37:14 -0500

    added test_account and added docstrings to account

--- account.py
+++ account.py
@@ -1,25 +1,47 @@
 class Account:
-    def __init__(self, name):
+    def __init__(self, name: str) -> None:
+        """
+        Function to set up account object
+        :param name: Account name
+        """
         self.account_name = name
         self.account_balance = 0
 
-    def deposit(self, amount):
+    def deposit(self, amount) -> bool:
+        """
+        Function to add to object
+        :param amount: Numeric value to add
+        :return: Boolean value to show if the value went through
+        """
         if amount > 0:
             self.account_balance += amount
             return True
         else:
             return False
 
-    def withdraw(self, amount):
+    def withdraw(self, amount) -> bool:
+        """
+        Function to subtract from object
+        :param amount: Numeric value to subtract
+        :return: Boolean value to show if the value went through
+        """
         if (amount > 0) and (amount <= self.account_balance):
             self.account_balance -= amount
             return True
         else:
             return False
 
-    def get_balance(self):
+    def get_balance(self) -> int:
+        """
+        Function to get the numeric value from object
+        :return: Numeric value of account_balance
+        """
         return self.account_balance
 
-    def get_name(self):
+    def get_name(self) -> str:
+        """
+        Function to get the value from object
+        :return: String value of account_name
+        """
         return self.account_name
     
---
+++ test_account.py
@@ -0,0 +1,35 @@
+from account import *
+
+class Test:
+    def setup_method(self):
+        self.a1 = Account('John')
+
+    def teardown_method(self):
+        del self.a1
+
+    def test_init(self):
+        assert self.a1.get_name() == 'John'
+
+    def test_deposit(self):
+        assert self.a1.deposit(-1.5) is False
+        assert self.a1.get_balance() == 0
+
+        assert self.a1.deposit(0) is False
+        assert self.a1.get_balance() == 0
+
+        assert self.a1.deposit(60) is True
+        assert self.a1.get_balance() == 60
+
+    def test_withdraw(self):
+        assert self.a1.withdraw(-25) is False
+        assert self.a1.get_balance() == 0
+
+        assert self.a1.withdraw(0) is False
+        assert self.a1.get_balance() == 0
+
+        assert self.a1.withdraw(25) is False
+        assert self.a1.get_balance() == 0
+
+        assert self.a1.deposit(25) is True
+        assert self.a1.withdraw(25) is True
+        assert self.a1.get_balance() == 0
